3e7aeb10fd55767c3961dd7c2b38361c9602bf17
Prajyot5135 <130391558+prajyotpawar5135@users.noreply.github.com>
2023-06-17 22:09:12 +0530

    ~Enum basics

---
+++ Demo_enum/src/com/app/Product.java
@@ -0,0 +1,27 @@
+package com.app;
+
+public class Product {
+
+	
+	private int prdid;
+	private String name;
+	private double price;
+	private Category category;
+	
+	public Product(int prdid,String name,double price,Category category)
+	{
+		
+		this.prdid=prdid;
+		this.name=name;
+		this.price=price;
+		this.category=category;
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Product [prdid=" + prdid + ", name=" + name + ", price=" + price + ", category=" + category + "]";
+	}
+	
+	
+}
